7bbb4d87088e49fe83238fa6c6b23d383b7f0932
Rafnes <rafzograf@gmail.com>
2024-09-11 14:19:39 +0300

    initial commit

---
+++ src/main/java/pro/sky/Shopie/ShopieApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.Shopie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopieApplication.class, args);
+	}
+
+}
